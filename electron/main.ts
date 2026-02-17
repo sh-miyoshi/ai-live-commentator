1ac457dd3bc72149a5bde75bc4353c2aa98914d7
s.miyoshi <s.miyoshi1991@gmail.com>
2026-02-17 22:15:26 +0900

    chore: update dependencies in package.json
    - Added @google/genai version 1.41.0
    - Downgraded react-intl from version 8.1.3 to 7.0.4

--- electron/main.ts
+++ electron/main.ts
@@ -1,9 +1,14 @@
 import { app, BrowserWindow, ipcMain } from 'electron'
 import path from 'node:path'
 import ollama from 'ollama'
+import { GoogleGenAI } from '@google/genai'
 
 const DEV_URL = 'http://localhost:5173'
+
 const OLLAMA_MODEL_NAME = 'qwen3:4b'
+
+const GEMINI_API_KEY = '' // 設定されている場合geminiを使います
+const GEMINI_MODEL_NAME = 'gemini-3-flash-preview'
 
 function createWindow () {
   const win = new BrowserWindow({
@@ -39,25 +44,45 @@ app.on('window-all-closed', () => {
 ipcMain.handle(
   'chat',
   async (_, payload: { title: string; context: string }) => {
+    let model = 'ollama'
+    if (GEMINI_API_KEY != '') {
+      model = 'gemini'
+    }
     console.log('title: ', payload.title)
     console.log('context: ', payload.context)
 
-    const response = await ollama.chat({
-      model: OLLAMA_MODEL_NAME,
-      messages: [
-        {
-          role: 'system',
-          content: `あなたは「${payload.title}」という配信の視聴者です。`
-        },
-        {
-          role: 'user',
-          content: `配信主は最後に「${payload.context}」と言ってるのでその回答を考えてください。回答は1文で簡潔にしてください。なお、チャットメッセージの部分のみ出力してください。`
-        }
-      ]
-    })
+    let message = ''
+    if (model === 'ollama') {
+      // OLLAMA
+      const response = await ollama.chat({
+        model: OLLAMA_MODEL_NAME,
+        messages: [
+          {
+            role: 'system',
+            content: `あなたは「${payload.title}」という配信の視聴者です。`
+          },
+          {
+            role: 'user',
+            content: `配信主は最後に「${payload.context}」と言ってるのでその回答を考えてください。回答は1文で簡潔にしてください。なお、チャットメッセージの部分のみ出力してください。`
+          }
+        ]
+      })
+      console.log('ollama response: ', response.message)
+      message = response.message.content
+    } else if (model === 'gemini') {
+      // Gemini
+      const ai = new GoogleGenAI({
+        apiKey: GEMINI_API_KEY
+      })
 
-    console.log('ollama response: ', response.message)
-    const message = response.message.content
+      const response = await ai.models.generateContent({
+        model: GEMINI_MODEL_NAME,
+        contents: `あなたは「${payload.title}」という配信の視聴者です。配信主は最後に「${payload.context}」と言ってるのでその回答を考えてください。回答は1文で簡潔にしてください。なお、チャットメッセージの部分のみ出力してください。`
+      })
+
+      console.log('gemini response: ', response)
+      message = response.text || ''
+    }
 
     return [
       {
